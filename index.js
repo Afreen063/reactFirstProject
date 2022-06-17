4a653f6d97055020cd1dbe2b08a0dc1652f723eb
Afreen063 <afreen0501@gmail.com>
2022-06-17 18:04:22 +0530

    first static react

---
+++ index.js
@@ -0,0 +1,11 @@
+//ReactDOM.render(<h1>Hello everyone</h1>,document.getElementById("root"));
+// ReactDOM is global variable which help us to render html. 
+//render( <html content>, where we want to render this html content) .. render() is a function.
+//ReactDOM.render(<p>Hello everyone</p>,document.getElementById("root"));
+
+
+ReactDOM.render(<ul>
+    <li>good</li>
+    <li>bad</li>
+</ul>, document.getElementById("root"));
+// we can also add unordered list .
