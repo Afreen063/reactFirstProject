86e8b7e86e84a1db4af537239d5aaaf77cb749b4
Afreen063 <afreen0501@gmail.com>
2022-06-19 19:26:17 +0530

    made component

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ function Page(){
 
   return (
   <div>
-    <header>
-    <img src ="https://tse2.mm.bing.net/th?id=OIP.8MEfXloFsCOJm3CezNPBcAHaGm&pid=Api&P=0&w=196&h=174" width="100px"></img>
-    </header>
-    <h1>
+     <Header /> 
+   <Main />
+   <Footer />
+  </div>
+)
+}
+function Main()
+{
+  return (
+    <div>
+       <h1>
       hi there is my first react page
     </h1>
     <ol>
@@ -13,13 +20,29 @@ function Page(){
       <li>copy babel in the script</li>
       <li>write type=text/babel</li>
     </ol>
+    </div>
+  )
+}
+function Footer()
+{
+  return (
     <footer>
-  0 20xx Ansari development. All right reserved.
-    </footer>
-  </div>
-)
+    0 20xx Ansari development. All right reserved.
+      </footer>
+  )
+}
+
+function Header(){
+  return(
+    <header>
+    <nav>
+      <img src ="https://tse2.mm.bing.net/th?id=OIP.8MEfXloFsCOJm3CezNPBcAHaGm&pid=Api&P=0&w=196&h=174" width="100px"></img>
+    </nav>
+    </header>
+  )
 }
 
 ReactDOM.render(<Page />,document.getElementById("root"));
+
 
 
